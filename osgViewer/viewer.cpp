7e16812c0f3b438fc8adda8013b4335153f7f481
Vladimir Zarypov <krre31@gmail.com>
2015-10-07 10:44:00 +0300

    changed Viewer class to inherit from QQuickFramebufferObject

--- osgViewer/viewer.cpp
+++ osgViewer/viewer.cpp
@@ -1,7 +1,21 @@
 #include "viewer.h"
+#include <QtGui/QOpenGLFramebufferObject>
+#include <QtQuick/QQuickWindow>
+#include <qsgsimpletexturenode.h>
 
-Viewer::Viewer()
-{
-
+void ViewerRenderer::render() {
+    qDebug() << "render";
+//    update();
 }
 
+QOpenGLFramebufferObject *ViewerRenderer::createFramebufferObject(const QSize &size) {
+    QOpenGLFramebufferObjectFormat format;
+    format.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
+//    format.setSamples(4);
+    return new QOpenGLFramebufferObject(size, format);
+}
+
+QQuickFramebufferObject::Renderer* Viewer::createRenderer() const
+{
+    return new ViewerRenderer();
+}
--- osgViewer/viewer.h
+++ osgViewer/viewer.h
@@ -1,10 +1,18 @@
 #pragma once
-#include <QtQuick>
+#include <QtQuick/QQuickFramebufferObject>
 
-class Viewer : public QQuickItem
+class ViewerRenderer : public QQuickFramebufferObject::Renderer
+{
+public:
+    ViewerRenderer() {}
+    void render();
+    QOpenGLFramebufferObject *createFramebufferObject(const QSize &size);
+};
+
+class Viewer : public QQuickFramebufferObject
 {
     Q_OBJECT
 
 public:
-    Viewer();
+    Renderer* createRenderer() const;
 };
