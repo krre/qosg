7733313263176921b5832314ad938708b767dca6
Vladimir Zarypov <krre31@gmail.com>
2015-10-08 16:52:36 +0300

    added QQmlListProperty<Drawable> to Geode class

--- osg/geode.cpp
+++ osg/geode.cpp
@@ -13,3 +13,32 @@ void Geode::classBegin()
     geode = static_cast<osg::Geode*>(obj);
     Node::classBegin();
 }
+
+void Geode::append(QQmlListProperty<Drawable>* list, Drawable* drawable)
+{
+    if (!drawable) {
+        return;
+    }
+    Geode* self = static_cast<Geode*>(list->object);
+    self->geode->addDrawable(drawable->instance());
+    self->drawableListChanged();
+}
+
+Drawable* Geode::at(QQmlListProperty<Drawable>* list, int index)
+{
+    Geode* self = static_cast<Geode*>(list->object);
+    return self->geode->getDrawable(index);
+}
+
+int Geode::count(QQmlListProperty<Drawable>* list)
+{
+    Geode* self = static_cast<Geode*>(list->object);
+    return self->geode->getNumDrawables();
+}
+
+void Geode::clear(QQmlListProperty<Drawable>* list)
+{
+    Geode* self = static_cast<Geode*>(list->object);
+    self->geode->removeDrawables(0, self->geode->getNumDrawables());
+    self->drawableListChanged();
+}
--- osg/geode.h
+++ osg/geode.h
@@ -1,17 +1,33 @@
 #pragma once
 #include <QtCore>
-#include "node.h"
 #include <osg/Geode>
+#include "node.h"
+#include "drawable.h"
+
+
 
 class Geode : public Node
 {
     Q_OBJECT
+    Q_PROPERTY(QQmlListProperty<Drawable> drawableList READ drawableList NOTIFY drawableListChanged)
+    Q_CLASSINFO("DefaultProperty", "drawableList")
 
 public:
     Geode();
     void classBegin() override;
     osg::Geode* instance() { return geode; }
 
+    QQmlListProperty<Drawable> drawableList() { return QQmlListProperty<Drawable>(this, 0, Geode::append, Geode::count, Geode::at, Geode::clear); }
+
+signals:
+    void drawableListChanged();
+
 private:
     osg::Geode* geode;
+
+    static void append(QQmlListProperty<Drawable>* list, Drawable* drawable);
+    static Drawable* at(QQmlListProperty<Drawable>* list, int index);
+    static int count(QQmlListProperty<Drawable>* list);
+    static void clear(QQmlListProperty<Drawable>* list);
+//    QQmlListProperty<Drawable> m_drawableList;
 };
