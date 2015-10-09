2efdf846b2a04fa5a4c47afd60f3fed899a2bcc5
Vladimir Zarypov <krre31@gmail.com>
2015-10-09 12:21:27 +0300

    added text property to Text class

--- osgText/textbase.cpp
+++ osgText/textbase.cpp
@@ -4,3 +4,11 @@ void TextBase::classBegin()
 {
     Drawable::classBegin();
 }
+
+void TextBase::setText(QString text)
+{
+    if (toOsg()->getText().createUTF8EncodedString() == text.toStdString()) return;
+
+    toOsg()->setText(text.toStdString());
+    emit textChanged(text);
+}
--- osgText/textbase.h
+++ osgText/textbase.h
@@ -5,8 +5,14 @@
 class TextBase : public Drawable
 {
     Q_OBJECT
+    Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
 public:
     TextBase() {}
     void classBegin() override;
     osgText::TextBase* toOsg() { return static_cast<osgText::TextBase*>(osgObj); }
+
+    QString text() { return QString::fromStdString((toOsg()->getText().createUTF8EncodedString())); }
+    void setText(QString text);
+signals:
+    void textChanged(QString text);
 };
