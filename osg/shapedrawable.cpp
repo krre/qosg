ee14b601ec676a0cb58f6216f98c5a0d4bc1aaac
Vladimir Zarypov <krre31@gmail.com>
2015-10-09 08:49:24 +0300

    added shape property to ShapeDrawable class

--- osg/shapedrawable.cpp
+++ osg/shapedrawable.cpp
@@ -1,2 +1,11 @@
 #include "shapedrawable.h"
+#include <QDebug>
 
+void ShapeDrawable::setShape(Shape* shape)
+{
+    if (static_cast<Shape*>(getShape()) == shape) return;
+    osg::ShapeDrawable::setShape(static_cast<osg::Shape*>(shape));
+    setColor(osg::Vec4(1, 1, 0, 1));
+//    qDebug() << shape;
+    emit shapeChanged(shape);
+}
--- osg/shapedrawable.h
+++ osg/shapedrawable.h
@@ -1,11 +1,17 @@
 #pragma once
 #include <osg/ShapeDrawable>
 #include "drawable.h"
+#include "shape.h"
 
 class ShapeDrawable : public Drawable, public osg::ShapeDrawable
 {
     Q_OBJECT
-
+    Q_PROPERTY(Shape* shape READ getShape WRITE setShape NOTIFY shapeChanged)
 public:
     ShapeDrawable() {}
+
+    Shape* getShape() { return static_cast<Shape*>(osg::ShapeDrawable::getShape()); }
+    void setShape(Shape* shape);
+signals:
+    void shapeChanged(Shape* shape);
 };
