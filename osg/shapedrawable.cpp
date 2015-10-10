a5499f44bab36e0f83a7af66f4867c08fe1e6933
Vladimir Zarypov <krre31@gmail.com>
2015-10-10 11:22:16 +0300

    added color property to ShapeDrawable class

--- osg/shapedrawable.cpp
+++ osg/shapedrawable.cpp
@@ -8,3 +8,11 @@ void ShapeDrawable::setShape(Shape* shape)
     toOsg()->setShape(shape->toOsg());
     emit shapeChanged(shape);
 }
+
+void ShapeDrawable::setColor(const QColor &color)
+{
+    osg::Vec4 vec4 = Converter::toVec4(color);
+    if (vec4 == toOsg()->getColor()) return;
+    toOsg()->setColor(vec4);
+    emit colorChanged(color);
+}
--- osg/shapedrawable.h
+++ osg/shapedrawable.h
@@ -2,11 +2,13 @@
 #include <osg/ShapeDrawable>
 #include "drawable.h"
 #include "shape.h"
+#include "../converter.h"
 
 class ShapeDrawable : public Drawable
 {
     Q_OBJECT
     Q_PROPERTY(Shape* shape READ getShape WRITE setShape NOTIFY shapeChanged)
+    Q_PROPERTY(QColor color READ getColor WRITE setColor NOTIFY colorChanged)
 public:
     ShapeDrawable() {}
     void classBegin() override { osgObj = new osg::ShapeDrawable; }
@@ -14,8 +16,13 @@ public:
 
     Shape* getShape() { return shape; }
     void setShape(Shape *shape);
+    QColor getColor() { return Converter::fromVec4(toOsg()->getColor()); }
+    void setColor(const QColor& color);
+
 signals:
     void shapeChanged(Shape* shape);
+    void colorChanged(QColor color);
+
 private:
     Shape* shape;
 };
