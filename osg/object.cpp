2a312df3aeaf6c0ae5794a96aa959dfc2cd995e3
Vladimir Zarypov <krre31@gmail.com>
2015-10-12 09:51:23 +0300

    added name property to Object class

--- osg/object.cpp
+++ osg/object.cpp
@@ -1,1 +1,8 @@
 #include "object.h"
+
+void Object::setName(const QString& name)
+{
+    if (toOsg()->getName() == name.toStdString()) return;
+    toOsg()->setName(name.toStdString());
+    emit nameChanged(name);
+}
--- osg/object.h
+++ osg/object.h
@@ -2,17 +2,24 @@
 #include <QQmlParserStatus>
 #include <osg/Object>
 #include <QObject>
+#include "../converter.h"
 
 class Object : public QObject, public QQmlParserStatus
 {
     Q_OBJECT
     Q_INTERFACES(QQmlParserStatus)
-
+    Q_PROPERTY(QString name READ getName WRITE setName NOTIFY nameChanged)
 public:
     Object() {}
     void classBegin() override {}
     void componentComplete() override {}
     osg::Object* toOsg() { return osgObj.get(); }
+
+    QString getName() { return Converter::fromOsgString(toOsg()->getName()); }
+    void setName(const QString& name);
+
+signals:
+    void nameChanged(QString name);
 
 protected:
     osg::ref_ptr<osg::Object> osgObj;
