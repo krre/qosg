60b4fad11de43bce40cad059802d3ceef2cfdff4
Vladimir Zarypov <krre31@gmail.com>
2015-10-14 10:52:53 +0300

    added position property to PositionAttitudeTransform class

--- osg/positionattitudetransform.cpp
+++ osg/positionattitudetransform.cpp
@@ -7,3 +7,11 @@ void PositionAttitudeTransform::classBegin()
     }
     Transform::classBegin();
 }
+
+void PositionAttitudeTransform::setPosition(const QVector3D& position)
+{
+    osg::Vec3 vec3 = Converter::toVec3(position);
+    if (toOsg()->getPosition() == vec3) return;
+    toOsg()->setPosition(vec3);
+    emit positionChanged(position);
+}
--- osg/positionattitudetransform.h
+++ osg/positionattitudetransform.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <osg/PositionAttitudeTransform>
 #include "transform.h"
+#include "../converter.h"
 
 class PositionAttitudeTransform : public Transform
 {
     Q_OBJECT
+    Q_PROPERTY(QVector3D position READ getPosition WRITE setPosition NOTIFY positionChanged)
 public:
     PositionAttitudeTransform() {}
     void classBegin() override;
     osg::PositionAttitudeTransform* toOsg() { return static_cast<osg::PositionAttitudeTransform*>(osgObj.get()); }
+
+    QVector3D getPosition() { return Converter::fromVec3(toOsg()->getPosition()); }
+    void setPosition(const QVector3D& position);
+
+signals:
+    void positionChanged(const QVector3D& position);
 };
