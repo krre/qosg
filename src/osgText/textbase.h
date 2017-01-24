be7a9ded1e6a636634f913cab96997599cd56bb3
Vladimir Zarypov <krre31@gmail.com>
2017-01-24 08:08:08 +0200

    Rename TextBase::text() getter to getText()

--- src/osgText/textbase.h
+++ src/osgText/textbase.h
@@ -5,7 +5,7 @@
 
 class TextBase : public Drawable {
     Q_OBJECT
-    Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
+    Q_PROPERTY(QString text READ getText WRITE setText NOTIFY textChanged)
     Q_PROPERTY(QColor color READ getColor WRITE setColor NOTIFY colorChanged)
     Q_PROPERTY(float characterSize READ getCharacterSize WRITE setCharacterSize NOTIFY characterSizeChanged)
     Q_PROPERTY(QVector3D position READ getPosition WRITE setPosition NOTIFY positionChanged)
@@ -14,7 +14,7 @@ public:
     void classBegin() override;
     osgText::TextBase* toOsg() { return static_cast<osgText::TextBase*>(osgObj.get()); }
 
-    QString text() { return Converter::fromOsgString(toOsg()->getText()); }
+    QString getText() { return Converter::fromOsgString(toOsg()->getText()); }
     void setText(const QString& text);
 
     QColor getColor() { return Converter::fromVec4(toOsg()->getColor()); }
