95decd4bde2fee726d78655567da4450b41d559b
Vladimir Zarypov <krre31@gmail.com>
2017-01-24 08:06:06 +0200

    Remove unused Text3D::m_characterDepth member

--- src/osgText/text3d.h
+++ src/osgText/text3d.h
@@ -4,14 +4,13 @@
 
 class Text3D : public TextBase {
     Q_OBJECT
-    Q_PROPERTY(float characterDepth READ characterDepth WRITE setCharacterDepth NOTIFY characterDepthChanged)
-    float m_characterDepth;
+    Q_PROPERTY(float characterDepth READ getCharacterDepth WRITE setCharacterDepth NOTIFY characterDepthChanged)
 
 public:
     void classBegin() override;
     osgText::Text3D* toOsg() { return static_cast<osgText::Text3D*>(osgObj.get()); }
 
-    float characterDepth() { return toOsg()->getCharacterDepth(); }
+    float getCharacterDepth() { return toOsg()->getCharacterDepth(); }
     void setCharacterDepth(float characterDepth);
 signals:
     void characterDepthChanged(float characterDepth);
